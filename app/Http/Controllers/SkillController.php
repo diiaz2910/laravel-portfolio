3fc57050db84d4ecbddfa7601dc3dc494e6b34e4
Ricardo <diiaz2910@gmail.com>
2023-11-14 16:48:51 -0500

    EDIT VIEW WORKING

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -52,19 +52,11 @@ class SkillController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Skill $skill)
     {
-        //
+        return Inertia::render('Skills/Edit', compact('skill'));
     }
 
     /**
